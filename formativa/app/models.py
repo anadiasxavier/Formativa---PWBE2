b9f64bc93cb151736f008f0181bbfa50d6de8951
anadiasxavier <anadiasxavier07@gmail.com>
2025-06-12 16:55:17 -0300

    mundanças para o front

--- formativa/app/models.py
+++ formativa/app/models.py
@@ -13,7 +13,7 @@ class Usuario(AbstractUser):
     data_nascimento = models.DateField()
     data_contratacao = models.DateField()
 
-    REQUIRED_FIELDS = ['ni', 'data_nascimento', 'data_contratacao']
+    REQUIRED_FIELDS = ['ni', 'data_nascimento', 'data_contratacao', 'tipo']
 
     def __str__(self):
         return f'{self.username} ({self.get_tipo_display()})'
